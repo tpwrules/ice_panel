06773fcddd9426c4015a70d10a877094a50e5299
Thomas Watson <twatson52@icloud.com>
2019-11-11 18:29:53 -0600

    implement questionably working UART

---
+++ boneless_uart.py
@@ -0,0 +1,286 @@
+from nmigen import *
+from nmigen_boards.icebreaker import *
+
+# boneless CPU architecture stuff
+from boneless.gateware import ALSRU_4LUT, CoreFSM
+from boneless.arch.opcode import Instr
+from boneless.arch.opcode import *
+
+import pmod_resources
+from hub75 import PanelDescription, GammaParameters, FramebufferedHUB75Driver
+from pll import PLL
+import uart
+
+
+class BonelessLED(Elaboratable):
+    def __init__(self, panel_desc, led_domain="sync"):
+        self.pd = panel_desc
+        self.gp = GammaParameters(gamma=2.5, bpp=8) 
+
+        self.panel = FramebufferedHUB75Driver(self.pd, led_domain=led_domain,
+           gamma_params=self.gp)
+
+        self.cpu_rom = Memory(width=16, depth=256,
+            init=Instr.assemble(firmware(self.gp.bpp)))
+        self.cpu_core = CoreFSM(alsru_cls=ALSRU_4LUT, memory=self.cpu_rom)
+
+        self.uart = uart.SimpleUART(
+            default_divisor=uart.calculate_divisor(12e6, 9600))
+
+    def elaborate(self, platform):
+        platform.add_resources(pmod_resources.hub75_pmod)
+
+        uart_pins = platform.request("uart")
+
+        m = Module()
+        m.submodules.panel = panel = self.panel
+        m.submodules.cpu_core = cpu_core = self.cpu_core
+        m.submodules.uart = uart = self.uart
+
+        # split up the external bus
+        uart_en = Signal()
+        panel_en = Signal()
+        m.d.comb += [
+            uart_en.eq(cpu_core.o_bus_addr[-1] == 0),
+            panel_en.eq(cpu_core.o_bus_addr[-1] == 1),
+        ]
+
+        # hook up the CPU's external bus to the panel engine
+        m.d.sync += [
+            panel.i_we.eq(cpu_core.o_ext_we & panel_en),
+            panel.i_waddr.eq(cpu_core.o_bus_addr[:self.pd.chan_bits]),
+            panel.i_wdata.eq(cpu_core.o_ext_data[:self.gp.bpp])
+        ]
+
+        # then to the UART
+        m.d.sync += [
+            uart.i_re.eq(cpu_core.o_ext_re & uart_en),
+            uart.i_we.eq(cpu_core.o_ext_we & uart_en),
+            uart.i_addr.eq(cpu_core.o_bus_addr[:2]),
+            uart.i_wdata.eq(cpu_core.o_ext_data),
+            cpu_core.i_ext_data.eq(uart.o_rdata),
+        ]
+
+        m.d.comb += [
+            uart.i_rx.eq(uart_pins.rx),
+            uart_pins.tx.eq(uart.o_tx),
+        ]
+
+        return m
+
+def firmware(bpp):
+    # time between color changes
+    # 1 second of clocks / (4 clocks per insn * 3 delay loop insns)
+    period = 1#120000//(4*3)
+    curr_color = R7
+    curr_font_data = R6
+    curr_msg_ptr = R5
+    curr_fb_ptr = R4
+    ch_pixels_remaining = R3
+    proc_ptr = R2
+    temp1 = R0
+    temp2 = R1
+
+    def fw_uart():
+        curr_char = R6
+        curr_msg_ptr = R5
+        temp1 = R4
+        temp2 = R3
+
+        return [
+            MOVI(curr_msg_ptr, 0),
+        L("sendch"),
+            LDR(curr_char, curr_msg_ptr, "message"),
+        L("busy"), # loop while the UART doesn't have space for another char
+            LDXA(temp1, 2),
+            ANDI(temp1, temp1, 0x8000),
+            BNZ("busy"),
+            ADDI(curr_char, curr_char, ord('d')),
+            STXA(curr_char, 2),
+            ADDI(curr_msg_ptr, curr_msg_ptr, 1),
+            CMPI(curr_msg_ptr, 10),
+            BNE("sendch"),
+        ]
+
+    fw = [ #fw_uart(),
+        L("begin"),
+        MOVI(curr_color, ord("A")),
+        STXA(curr_color, 2),
+    L("full"),
+        LDXA(curr_color, 2),
+        ANDI(curr_color, curr_color, 0x8000),
+        BNZ("full"),
+        J("begin"),
+        MOVI(curr_color, 0),
+    L("display_msg"),
+        # draw first half of message on top half of screen
+        MOVI(curr_msg_ptr, 0),
+        MOVI(curr_fb_ptr, 0x8000), # 0 is top left
+        # we don't have a free register to keep track of where we are in the
+        # message independently of where we are in the half message. we just
+        # modify the offset of the message character load instruction to switch
+        # halves.
+        MOVR(temp1, "msg_ch_load_insn"),
+        LD(temp2, temp1, 0),
+        ANDI(temp2, temp2, 0x1f ^ 0xFFFF), # clear offset to 0
+        ST(temp2, temp1, 0),
+        # then go and display that half
+        JAL(proc_ptr, "display_msg_row"),
+
+        # now the bottom row
+        MOVI(curr_msg_ptr, 0),
+        MOVI(curr_fb_ptr, 0x8000+(32*8*4)),
+        # replace load offset with 5 to get second half of message
+        MOVR(temp1, "msg_ch_load_insn"),
+        LD(temp2, temp1, 0),
+        ORI(temp2, temp2, 5), # set offset to 5
+        ST(temp2, temp1, 0),
+        JAL(proc_ptr, "display_msg_row"),
+
+    L("busy2"), # loop while the UART doesn't have space for another char
+        LDXA(temp1, 2),
+        ANDI(temp1, temp1, 0x8000),
+        BNZ("busy2"),
+        MOVI(temp1, ord("A")),
+        STXA(temp1, 2),
+
+        # wait some time for the message to show
+        MOVI(temp1, period&0xffff),
+        MOVI(temp2, (period>>16)+1),
+
+
+    L("delay"),
+        SUBI(temp1, temp1, 1),
+        SBCI(temp2, temp2, 0),
+        BNZ ("delay"),
+
+        # then switch colors and do it again
+        ADDI(curr_color, curr_color, 1),
+        J   ("display_msg"),
+
+    L("display_msg_row"),
+        # we write to the display one row at a time to save index pointers.
+        # row 0 of character 0, row 0 of character 1, ..., row 1 of character 0,
+        # row 1 of character 1, ..., row 7 of character 4.
+
+        # in the framebuffer, for a 32x16 panel, there are 4 color words * 32
+        # columns * 16 rows. 
+
+        # load the current character of the message
+        MOVR(temp1, "message"),
+        ADD(temp1, temp1, curr_msg_ptr),
+    L("msg_ch_load_insn"),
+        LD(temp1, temp1, 0), # offset gets modified!!!
+        # then the corresponding row of the font bitmap
+        SLLI(temp1, temp1, 3), # low 3 bits are row, remaining are character
+        ANDI(temp2, curr_fb_ptr, 7<<7), # pull row out of framebuffer pointer
+        SRLI(temp2, temp2, 7),
+        OR(temp1, temp1, temp2),
+        LDR(curr_font_data, temp1, "fontdata"),
+        # font is six pixels wide
+        MOVI(ch_pixels_remaining, 6),
+    L("display_ch_row"),
+        MOVI(temp2, 0), # assume this pixel is off
+        ANDI(temp1, curr_font_data, 0x80), # but maybe it's on?
+        BZ("no_pix"), # skip drawing 0 pixels to avoid filling up FIFO
+         # oh wait it is. ramp color up and down all cool-like
+        ANDI(temp2, curr_color, 0xFF),
+        ANDI(temp1, curr_color, 0x100),
+        BZ("display_ch_row_pix_off"),
+        SUBI(temp1, temp1, 1),
+        SUB(temp2, temp1, temp2),
+    L("display_ch_row_pix_off"),
+        # write the intensity as white to the frame buffer
+        STX(temp2, curr_fb_ptr, 0), # word 0 is red
+        STX(temp2, curr_fb_ptr, 1), # word 1 is green
+        STX(temp2, curr_fb_ptr, 2), # word 2 is blue
+    L("no_pix"),
+        # then move to the next pixel
+        ADDI(curr_fb_ptr, curr_fb_ptr, 4),
+        SLLI(curr_font_data, curr_font_data, 1),
+        # done with this character?
+        SUBI(ch_pixels_remaining, ch_pixels_remaining, 1),
+        BNZ("display_ch_row"),
+        # move to next character
+        ADDI(curr_msg_ptr, curr_msg_ptr, 1),
+        CMPI(curr_msg_ptr, 5), # done with this message half?
+        BNZ("display_msg_row"),
+        # we only draw 6*5=30 out of 32 pixels, thus skip the last 2 so we start
+        # at the next row.
+        ADDI(curr_fb_ptr, curr_fb_ptr, (32-(6*5))*4),
+        MOVI(curr_msg_ptr, 0), # start from first character on next row
+        ANDI(temp1, curr_fb_ptr, 7<<7), # done with all the rows?
+        BNZ("display_msg_row"),
+        JR(proc_ptr, 0),
+    ]
+    # adding data just consists of appending it to the instruction stream
+    fw.append(L("message"))
+    for m in "helloworld":
+        fw.append(ord(m)-ord('d'))
+
+    fw.append(L("fontdata"))
+
+    from font import tft_font
+    # the font bitmap is 6 pixels wide and 8 pixels high. bit 7 is the leftmost
+    # bit and bit 2 is the rightmost.
+
+    # we don't have enough free memory to hold a through z, so just store the
+    # character range needed for "hello world".
+    for ch in range(ord('d'), ord('w')+1):
+        for row in range(8):
+            fw.append(tft_font[ch*8+row])
+
+    return fw
+
+# super top domain to manage clock stuff
+class Top(Elaboratable):
+    def __init__(self, panel_desc, led_freq_mhz=12):
+        self.pd = panel_desc
+        self.led_freq_mhz = led_freq_mhz
+
+    def elaborate(self, platform):
+        m = Module()
+        if self.led_freq_mhz != 12:
+            # we need a PLL so we can boost the clock. reserve the clock pin
+            # before it gets switched to the default domain.
+            clk_pin = platform.request(platform.default_clk, dir="-")
+            # then create the PLL
+            pll = PLL(12, self.led_freq_mhz, clk_pin,
+                orig_domain_name="cpu", # runs at 12MHz
+                pll_domain_name="led", # runs at the LED frequency
+            )
+            m.submodules.pll = pll
+            led_domain = "led"
+        else:
+            # the user doesn't want to run faster and the PLL can't make
+            # input = output, so just create the CPU domain using the default
+            # clock and run the LEDs in that domain too.
+            cpu = ClockDomain("cpu")
+            m.domains += cpu
+            m.d.comb += ClockSignal("cpu").eq(ClockSignal("sync"))
+            led_domain = "cpu"
+
+        # create the actual demo and tell it to run in the domain we made
+        # for it above
+        boneless_led = BonelessLED(self.pd, led_domain=led_domain)
+
+        # remap the default sync domain to the CPU domain, since most logic
+        # should run there.
+        boneless_led = DomainRenamer("cpu")(boneless_led)
+
+        m.submodules.boneless_led = boneless_led
+
+        return m
+
+panel_desc = PanelDescription(width=32, height=16, bpp=10)
+
+if __name__ == "__main__":
+    from cli import main
+    def make(simulating):
+        design = Top(panel_desc=panel_desc,
+            # we can't simulate with different LED and CPU frequencies
+            led_freq_mhz=(12 if simulating else 40))
+        platform = ICEBreakerPlatform()
+        return design, platform
+
+    main(maker=make, build_args={"synth_opts": "-abc9"})
---
+++ uart.py
@@ -0,0 +1,225 @@
+# a UART for Boneless
+
+from nmigen import *
+from boneless.arch.opcode import *
+
+# Register Map
+
+# 0x0: (W) Baud Rate / (R) Status
+#   Write:   15-0: Baud Rate divisor. rate = ((input freq/2)/divisor)-1
+#    Read: bit 15: 1 if transmission in progress, 0 otherwise
+#          bit  0: 1 if reception in progress, 0 otherwise
+
+# 0x1: (R/W) Error
+#     R/W: bit 15: 1 if reception encountered framing error. write 1 to reset.
+#          bit  1: 1 if transmit FIFO overflowed. write 1 to reset.
+#          bit  0: 1 if receive FIFO overflowed. write 1 to reset.
+
+# 0x2: (R/W) Transmit Data / TX FIFO Status
+#    Read: bit 15: 1 if the TX FIFO is full, 0 otherwise.
+#   Write:    n-0: queue written character for transmission.
+
+# 0x3: (R) RX FIFO Status and Receive Data
+#    Read: bit 15: 1 if the RX FIFO is empty, 0 otherwise
+#             n-0: read character, if the RX FIFO is not empty
+
+class SetReset(Elaboratable):
+    def __init__(self, module, prefer_set=True):
+        # prefer_set: if True, set takes priority over reset
+        self.prefer_set = prefer_set
+
+        self.set = Signal()
+        self.reset = Signal()
+        self.value = Signal()
+
+        # add ourselves. removes the need for the user to add us.
+        module.submodules += self
+
+    def elaborate(self, platform):
+        m = Module()
+
+        if self.prefer_set:
+            with m.If(self.set):
+                m.d.sync += self.value.eq(1)
+            with m.Elif(self.reset):
+                m.d.sync += self.value.eq(0)
+        else:
+            with m.If(self.reset):
+                m.d.sync += self.value.eq(0)
+            with m.Elif(self.set):
+                m.d.sync += self.value.eq(1)
+
+        return m
+
+def calculate_divisor(freq, baud):
+    return int(freq/(2*baud))-1
+
+class SimpleUART(Elaboratable):
+    def __init__(self, default_divisor=0, char_bits=8):
+        self.default_divisor = default_divisor
+        self.char_bits = char_bits
+        if char_bits > 15 or char_bits < 1:
+            raise ValueError("char width '{}' not in 1-15".format(char_bits))
+
+        # boneless bus inputs. we only have four registers.
+        self.i_re = Signal()
+        self.i_we = Signal()
+        self.i_addr = Signal(2)
+        self.o_rdata = Signal(16)
+        self.i_wdata = Signal(16)
+
+        # UART signals
+        self.i_rx = Signal()
+        self.o_tx = Signal(reset=1) # inverted, like usual
+
+    def elaborate(self, platform):
+        m = Module()
+
+        # define the signals that make up the registers
+        r0_baud_divisor = Signal(16, reset=self.default_divisor)
+        r0_tx_active = Signal()
+        r0_rx_active = Signal()
+
+        r1_rx_error = SetReset(m, prefer_set=True)
+        r1_tx_overflow = SetReset(m, prefer_set=True)
+        r1_rx_overflow = SetReset(m, prefer_set=True)
+
+        r2_tx_full = SetReset(m, prefer_set=False)
+        r2_tx_data = Signal(self.char_bits)
+
+        r3_rx_empty = SetReset(m, prefer_set=False)
+        r3_rx_data = Signal(self.char_bits)
+
+
+        # handle the boneless bus.
+        read_data = Signal(16) # it expects one cycle of read latency
+        m.d.sync += self.o_rdata.eq(read_data)
+
+        with m.If(self.i_re):
+            with m.Switch(self.i_addr):
+                with m.Case(0): # status register
+                    m.d.comb += [
+                        read_data[15].eq(r0_tx_active),
+                        read_data[0].eq(r0_rx_active),
+                    ]
+                with m.Case(1): # error register
+                    m.d.comb += [
+                        read_data[15].eq(r1_rx_error.value),
+                        read_data[1].eq(r1_tx_overflow.value),
+                        read_data[0].eq(r1_rx_overflow.value),
+                    ]
+                with m.Case(2): # tx fifo status register
+                    m.d.comb += read_data[15].eq(r2_tx_full.value)
+                with m.Case(3): # rx fifo status + read data register
+                    # we don't really have a FIFO, just a buffer register and an
+                    # input shift register. so do FIFO-type logic here.
+                    m.d.comb += read_data[15].eq(r3_rx_empty.value)
+                    # even if the buffer is "empty", there's still bits there.
+                    # read them out so we don't have to have another mux.
+                    m.d.comb += read_data[:self.char_bits].eq(r3_rx_data)
+                    # and since we have read the only thing out of the buffer,
+                    # it's now empty
+                    m.d.comb += r3_rx_empty.set.eq(1)
+        with m.Elif(self.i_we):
+            with m.Switch(self.i_addr):
+                with m.Case(0): # baud rate register
+                    m.d.sync += r0_baud_divisor.eq(self.i_wdata)
+                with m.Case(1): # error register
+                    m.d.comb += [
+                        r1_rx_error.reset.eq(self.i_wdata[15]),
+                        r1_tx_overflow.reset.eq(self.i_wdata[1]),
+                        r1_rx_overflow.reset.eq(self.i_wdata[0]),
+                    ]
+                with m.Case(2): # transmit data register
+                    # we don't really have a FIFO, just a staging register and
+                    # an output shift register. so do FIFO-type logic here.
+                    with m.If(~r2_tx_full.value):
+                        m.d.sync += r2_tx_data.eq(
+                            self.i_wdata[:self.char_bits])
+                        # we've put something in the staging register, so we
+                        # can't accept anything else.
+                        m.d.comb += r2_tx_full.set.eq(1)
+                    with m.Else(): # overflowed! drop the write and raise error.
+                        m.d.comb += r1_tx_overflow.set.eq(1)
+
+
+        # transmit data (written in a function to keep locals under control)
+        def tx():
+            # count out the bits we're sending (including start and stop)
+            bit_ctr = Signal(range(self.char_bits+2))
+            # shift out the data bits and stop bit
+            out_buf = Signal(self.char_bits+1)
+            # count cycles per half baud
+            baud_ctr = Signal(16)
+
+            # send out a new bit when the state machine requests it
+            send_bit = Signal()
+            with m.If(send_bit):
+                m.d.sync += [
+                    self.o_tx.eq(out_buf[0]),
+                    out_buf.eq(out_buf >> 1),
+                ]
+
+            # automatically count down the time per half baud
+            baud_ctr_reset = Signal()
+            baud_ctr_done = Signal()
+            m.d.comb += baud_ctr_done.eq(baud_ctr == 0)
+            with m.If(baud_ctr_reset):
+                m.d.sync += baud_ctr.eq(r0_baud_divisor)
+            with m.Elif(~baud_ctr_done):
+                m.d.sync += baud_ctr.eq(baud_ctr-1)
+
+            with m.FSM("IDLE"):
+                with m.State("IDLE"):
+                    # once again, no real FIFO. if it's full, then there's
+                    # something to transmit.
+                    with m.If(r2_tx_full.value):
+                        # and once we start transmitting, it's empty.
+                        m.d.comb += r2_tx_full.reset.eq(1)
+                        m.d.sync += [
+                            # load data to send, plus stop bit
+                            out_buf.eq(Cat(r2_tx_data, 1)),
+                            # start counting down the bits
+                            bit_ctr.eq(self.char_bits+2-1),
+                            # send the start bit first
+                            self.o_tx.eq(0),
+                            # finally, let it be known that we are sending
+                            r0_tx_active.eq(1),
+                        ]
+                        # start counting down this baud time
+                        m.d.comb += baud_ctr_reset.eq(1)
+                        m.next = "BAUD0"
+
+                with m.State("BAUD0"):
+                    # nothing to do here, just passing the time...
+                    with m.If(baud_ctr_done):
+                        m.d.comb += baud_ctr_reset.eq(1)
+                        m.next = "BAUD1"
+
+                with m.State("BAUD1"):
+                    # but once the half baud timer expires here, we need to send
+                    # out a new bit.
+                    with m.If(baud_ctr_done):
+                        # are we on the last bit?
+                        with m.If(bit_ctr == 0):
+                            m.d.sync += [
+                                # yes, we are done!
+                                r0_tx_active.eq(0),
+                                # leave the bus idle
+                                self.o_tx.eq(0),
+                            ]
+                            m.next = "IDLE"
+                        with m.Else():
+                            # nope. shift out the next one and wait for the
+                            # appropriate time.
+                            m.d.comb += [
+                                send_bit.eq(1),
+                                baud_ctr_reset.eq(1),
+                            ]
+                            # one less bit to go
+                            m.d.sync += bit_ctr.eq(bit_ctr-1)
+                            m.next = "BAUD0"
+
+        tx()
+
+        return m
