e2015c1c21bb388530681e327be20d42a38fad60
Thomas Watson <twatson52@icloud.com>
2019-11-20 08:46:15 -0600

    split display generation and actual display up to make dev easier; also use the unit trig functions instead of tau ones

--- plasma_pygame.py
+++ plasma_pygame.py
@@ -1,9 +1,4 @@
-import pygame
-import os
-import itertools
-from math import sin, cos, pow, sqrt, pi
-import struct
-import sys
+from math import sin, cos, pow, sqrt, pi, tau
 
 SCALE = 16
 DISP = (32, 16)
@@ -21,46 +16,23 @@ def plasma3o(x, y, t):
 
 funcso = [plasma1o, plasma2o, plasma3o]
 
+def usin(x): return sin(x*tau)
+def ucos(x): return cos(x*tau)
+
 def plasma1(x, y, t):
-    return sin(x*10+t)
+    return usin(x*10+t)
 
 def plasma2(x, y, t):
-    return sin(10*(x*sin(t/2)+y*cos(t/3))+t)
+    return usin(10*(x*usin(t/2)+y*ucos(t/3))+t)
 
 def plasma3(x, y, t):
-    cx = x+.5*sin(t/5)
-    cy = y+.5*cos(t/3)
-    return sin(sqrt(100*(cx*cx+cy*cy)+1)+t)
+    cx = x*tau+.5*usin(t/5)
+    cy = y*tau+.5*ucos(t/3)
+    return usin(sqrt(100/(tau**2)*(cx*cx+cy*cy)+(1/(tau**2)))+t)
 
 funcs = [plasma1, plasma2, plasma3]
 
-
-output = []
-for x in range(DISP[1]):
-    output.append([(0, 0, 0)]*DISP[0])
-
-pygame.init()
-
-# open display
-screen = pygame.display.set_mode((SCALE*DISP[0], 2*SCALE*DISP[1]+SCALE))
-
-# now enter pygame event loop
-frame_clock = pygame.time.Clock()
-frame = 0
-while True:
-    button_changed = False
-    should_quit = True
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            break
-        elif event.type == pygame.KEYDOWN:
-            if event.key == "q":
-                break
-    else:
-        should_quit = False
-    if should_quit:
-        break
-
+def draw(screen, frame):
     # original version
     t = frame/30
     for y_ in range(DISP[1]):
@@ -74,17 +46,14 @@ while True:
                 (x_*SCALE, y_*SCALE, SCALE-4, SCALE-4))
 
     # modified, optimized version
+    t = frame/(30*tau)
     for y_ in range(DISP[1]):
-        y__ = (y_-(DISP[1])/2)/DISP[1]
+        y__ = ((y_-(DISP[1])/2)/DISP[1]) / tau
         for x_ in range(DISP[0]):
-            v = sum([f((x_-(DISP[0]/2))/DISP[0], y__, t) for f in funcs])
-            output = (int((sin(v*pi)+1)*255/2),
-                int((sin(v*pi+2*pi/3)+1)*255/2),
-                int((sin(v*pi+4*pi/3)+1)*255/2))
+            v = sum([f(((x_-(DISP[0]/2))/DISP[0]) / tau, y__, t) for f in funcs])
+            output = (int((usin(v*0.5)+1)*255/2),
+                int((usin(v*0.5+0.333333)+1)*255/2),
+                int((usin(v*0.5+0.666667)+1)*255/2))
             screen.fill(output,
                 (x_*SCALE, (y_+DISP[1]+1)*SCALE, SCALE-4, SCALE-4))
 
-    pygame.display.flip()
-    frame_clock.tick(30)
-
-    frame += 1
---
+++ pygame_engine.py
@@ -0,0 +1,51 @@
+import pygame
+import sys
+import importlib
+import traceback
+
+# import the display module the user wishes to use
+disp_mod = importlib.import_module(sys.argv[1].replace(".py", ""))
+SCALE = disp_mod.SCALE
+DISP = disp_mod.DISP
+
+pygame.init()
+
+# open display
+screen = pygame.display.set_mode((SCALE*DISP[0], 2*SCALE*DISP[1]+SCALE),
+    display=1)
+
+# now enter pygame event loop
+frame_clock = pygame.time.Clock()
+frame = 0
+while True:
+    button_changed = False
+    should_quit = True
+    should_reload = False
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            break
+        elif event.type == pygame.KEYDOWN:
+            if event.unicode == "q":
+                break
+            elif event.unicode == "r":
+                should_reload = True
+    else:
+        should_quit = False
+    if should_quit:
+        break
+
+    if should_reload:
+        # reload the display module cause presumably the user changed it
+        try:
+            importlib.reload(disp_mod)
+            # and start the whole deal over
+            frame = 0
+        except:
+            traceback.print_exc()
+
+    disp_mod.draw(screen, frame)
+
+    pygame.display.flip()
+    frame_clock.tick(30)
+
+    frame += 1
